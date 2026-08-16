58438bebc336d446bfd69e01909a57f0ea40abe6
Aditya Rauniyar <adityarauniyar.work@gmail.com>
2026-08-16 22:32:09 +0530

    Time: 0 ms (100%), Space: 17.2 MB (28.51%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -3,52 +3,46 @@ public:
 
     int dr[4]={-1,0,1,0};
     int dc[4]={0,1,0,-1};
+    queue<pair<int,int>>q;
 
     int orangesRotting(vector<vector<int>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
         int fresh=0;
-        queue<pair<int,int>>q;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
+                if(grid[i][j]==2){
+                    q.push({i,j});
+                }
                 if(grid[i][j]==1) fresh++;
-                if(grid[i][j]==2) q.push({i,j});
             }
         }
-
         if(!fresh) return 0;
-        int minutes=0;
-
         vector<vector<int>>vis(m,vector<int>(n,0));
-
-        while(!q.empty() && fresh>0){
-            int size=q.size();
-
-            for(int i=0;i<size;i++){
-                int r = q.front().first;
-                int c = q.front().second;
-                q.pop();
-
+        int minutes=0;
+            while(!q.empty()){
+              int s=q.size();
+              bool change=0;
+              for(int i=0;i<s;i++){
+                  auto [x,y]=q.front();
+                  q.pop();
                 for(int k=0;k<4;k++){
-                    int nr=r+dr[k];
-                    int nc=c+dc[k];
-
-                     if (nr >= 0 && nr < m &&
-                        nc >= 0 && nc < n &&
-                        grid[nr][nc] == 1) {
-
-                        grid[nr][nc] = 2;  
+                    int nr=x+dr[k];
+                    int nc=y+dc[k];
+                    if(nr>=0 && nr<m && nc>=0 && nc<n && !vis[nr][nc] && grid[nr][nc]==1){
+                        vis[nr][nc]=1;
+                        grid[nr][nc]=2;
+                        q.push({nr,nc});
                         fresh--;
-
-                        q.push({nr, nc});
+                        change=true;
                     }
                 }
+               }
+                if(change) minutes++;
             }
-            minutes++;
-        }
-
-        if(fresh>0) return -1;
+            
+    
+        if(fresh) return -1;
         return minutes;
-
     }
 };
